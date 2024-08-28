f2cc0a6253737b1a64f6612f2b69a2916861889b
Ashley Newman <ashleynew2122002@gmail.com>
2024-08-28 19:17:37 +0200

    Rails Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: :new_restaurant_review
+  # post "restaurants/:restaurant_id/reviews"
+end
   # Defines the root path route ("/")
   # root "posts#index"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+yummy = { name: "Yummy", address: "7 Boundary St, Franschoek", phone_number: "0213568933", category: "french" }
+vendettas = { name: "Vendettas", address: "56 Shore St, Cape Town", phone_number: "0832568963", category: "italian" }
+suki_thai = { name: "Suki Thai", address: "21 Bright St, Stellenbosch", phone_number: "0732566261", category: "chinese" }
+genki_sushi = { name: "Genki Sushi", address: "10 Victoria St, Stellenbosch", phone_number: "0622569003",  category: "japanese" }
+waffle_house = { name: "Waffle House", address: "42 Long St, Cape Town", phone_number: "0844528163", category: "belgian" }
+
+[yummy, vendettas, suki_thai, genki_sushi, waffle_house].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
